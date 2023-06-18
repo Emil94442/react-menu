15b0733b73f82ec3841c024ea5493d235ed83457
Emil <emil94442@gmail.com>
2023-06-18 18:43:39 +0600

    updated

--- src/Routes pages/Hadsome/Handsome.jsx
+++ src/Routes pages/Hadsome/Handsome.jsx
@@ -5,7 +5,7 @@ const Handsome = () => {
     <div className='Handsome'>
         <img src="./images/photo_2022-04-24_18-04-33.jpg" alt="" className='Emil-GD'/>
         <div className='Border-about-me'>
-            <div className='About-me'>Возвраст 17 лет <br /> Рост 184 см <br /> Телосложение: Худой</div>
+            <div className='About-me'>Возвраст 17 лет <br /> Рост 187 см <br /> Телосложение: Худой</div>
         </div>
     </div>
   )
